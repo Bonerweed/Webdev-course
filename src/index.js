d4f99d634eb39c30a1b25a1f0a524868665cc9a4
Vesa Koski <vesa.koski@student.lut.fi>
2021-09-23 15:30:10 +0300

    feat: week 2 assigment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 let commentList;
 let commentField;
+let ratingSelect;
 
 document.addEventListener("DOMContentLoaded", () => {
   initVals();
@@ -9,17 +10,36 @@ function initVals() {
   console.log("setting attributes");
   commentList = document.getElementById("comment-list");
   commentField = document.getElementById("comment-text");
+  ratingSelect = document.getElementById("rating");
 }
 
 function addComment () {
   console.log(commentField.value.trim().split(/\s+/).length, commentField.value.length);
   if (commentField && commentField.value.trim().split(/\s+/).length > 0 && commentField.value.length > 0) {
     const comment = document.createElement("li");
+    const divWrapper = document.createElement("div");
     const text = document.createElement("p");
+    const rating = document.createElement("p");
+    const delButton = document.createElement("button");
+
+    delButton.textContent = "Nuke This!";
+
     text.appendChild(document.createTextNode(commentField.value));
+    rating.appendChild(document.createTextNode(ratingSelect.value));
+
+    rating.classList.add("comment-rating");
+    text.classList.add("comment-text");
+    delButton.classList.add("nuke-button-invis");
     commentField.value = "";
-    comment.appendChild(text);
+    divWrapper.classList.add("comment");
+
+    divWrapper.appendChild(rating);
+    divWrapper.appendChild(text);
+    divWrapper.appendChild(delButton);
+    comment.appendChild(divWrapper);
     commentList.appendChild(comment);
+
+    delButton.addEventListener("click", function() {deleteThis(divWrapper)} );
   }
   else {
     console.log("too short", commentField.value.split(/\s+/), commentField.value);
@@ -27,11 +47,19 @@ function addComment () {
 }
 
 function clearComments () {
-  const confirmation = confirm("This will delete all comments, you sure?");
+  /*const confirmation = confirm("This will delete all comments, you sure?");
   if (confirmation) {
     commentList.innerHTML = "";
   }
   else {
     console.log("user was scared", confirmation);
+  }*/
+  const buttons = [...document.getElementsByClassName("nuke-button-invis")];
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("nuke-button-invis");
   }
+}
+
+function deleteThis (button) {
+  button.parentElement.remove();
 }
